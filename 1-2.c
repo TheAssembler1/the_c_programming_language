16ba7be651c154ccb8d783d3db02cb5771c86287
TheAssembler1 <lewisnoah592@gmail.com>
2022-01-30 19:36:10 -0600

    Currently working on 1-4 but things are going pretty smoothly so far

---
+++ 1-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main(){
+    printf("hello, ");
+    printf("world\n");
+    printf("--\n--");
+    printf("--\t--");
+    printf("--\b--");
+    printf("--\"--");
+    printf("--\\--");
+}
---
+++ 1-4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* print Farenheit-Celsius table
+   for fahr = 0, 0, ..., 300; floating-point version */
+main(){
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;   /* upper limit */
+    step = 20;      /* step size */
+   
+    printf("----------\n"); 
+    printf(" %-3s   %-5s\n", "F*", "C*");
+    printf("----------\n");
+
+    fahr = lower;
+    while(fahr <= upper){
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
